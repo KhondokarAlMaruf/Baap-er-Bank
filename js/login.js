f9f09638ea874e8a6db56e4da0b1707029390303
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-03-17 14:05:35 +0600

    js added

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const userEmail = document.getElementById('user-email');
+    const email = userEmail.value;
+    console.log(email);
+    const userPass = document.getElementById('user-pass');
+    const pass = userPass.value;
+    console.log(pass);
+    
+
+
+})
